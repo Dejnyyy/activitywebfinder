874d65378a8a64cf302a3aa1da1ff8b828782bb9
Dejnyyy <danslaby@email.cz>
2024-07-25 01:17:55 +0200

    neco takhle lepsi

--- src/pages/components/sidebar.tsx
+++ src/pages/components/sidebar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Image from 'next/image';
 import { CgAddR } from "react-icons/cg";
+import { IoIosNotifications } from "react-icons/io";
+
 
 const Sidebar = () => {
   return (
-    <div className="w-16 fixed h-full left-0 bg-white shadow-md">
+    <div className="w-16 fixed h-full left-0 shadow-md">
       <div>
         <Image
           className="mx-auto my-2 mb-4"
@@ -14,7 +16,8 @@ const Sidebar = () => {
           height={50}
         />
       </div>
-      <CgAddR className='text-4xl mx-auto my-4' />
+      <CgAddR className='text-4xl text-white mx-auto my-4' />
+      <IoIosNotifications className='text-4xl text-white mx-auto my-4' />
 
     </div>
   );
