13201f7fa0600d124d8683f022b39c785b082eb9
Dejnyyy <danslaby@email.cz>
2024-07-25 01:33:19 +0200

    toe popici

--- src/pages/components/sidebar.tsx
+++ src/pages/components/sidebar.tsx
@@ -2,23 +2,55 @@ import React from 'react';
 import Image from 'next/image';
 import { CgAddR } from "react-icons/cg";
 import { IoIosNotifications } from "react-icons/io";
-
+import { FaMap } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
 
 const Sidebar = () => {
   return (
-    <div className="w-16 fixed h-full left-0 shadow-md">
-      <div>
+    <div className="w-16 fixed h-full left-0 bg-gray-900 shadow-lg flex flex-col items-center py-4">
+      <div className="mb-6">
         <Image
-          className="mx-auto my-2 mb-4"
+          className="mx-auto"
           src="/favicon.ico"
           alt="Logo"
           width={50}
           height={50}
         />
       </div>
-      <CgAddR className='text-4xl text-white mx-auto my-4' />
-      <IoIosNotifications className='text-4xl text-white mx-auto my-4' />
-
+      <nav className="flex flex-col space-y-8 mt-4">
+        <div className="relative group">
+          <a href="#" className="text-white hover:text-gray-400">
+            <FaHome className='text-4xl' />
+          </a>
+          <span className="absolute left-16 top-1/2 transform -translate-y-1/2 w-max bg-gray-800 text-white text-xs rounded-lg px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
+            Home
+          </span>
+        </div>
+        <div className="relative group">
+          <a href="#" className="text-white hover:text-gray-400">
+            <FaMap className='text-4xl' />
+          </a>
+          <span className="absolute left-16 top-1/2 transform -translate-y-1/2 w-max bg-gray-800 text-white text-xs rounded-lg px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
+            Map
+          </span>
+        </div>
+        <div className="relative group">
+          <a href="#" className="text-white hover:text-gray-400">
+            <IoIosNotifications className='text-4xl' />
+          </a>
+          <span className="absolute left-16 top-1/2 transform -translate-y-1/2 w-max bg-gray-800 text-white text-xs rounded-lg px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
+            Notifications
+          </span>
+        </div>
+        <div className="relative group">
+          <a href="#" className="text-white hover:text-gray-400">
+            <CgAddR className='text-4xl' />
+          </a>
+          <span className="absolute left-16 top-1/2 transform -translate-y-1/2 w-max bg-gray-800 text-white text-xs rounded-lg px-2 py-1 opacity-0 group-hover:opacity-100 transition-opacity">
+            Add
+          </span>
+        </div>
+      </nav>
     </div>
   );
 };
