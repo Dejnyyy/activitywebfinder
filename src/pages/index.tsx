527aad8489e2d3f2b3ddd88cc5c789bfdf5ce8a8
Dejnyyy <danslaby@email.cz>
2024-07-25 02:14:38 +0200

    pokracujeme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import Sidebar from './components/sidebar';
+import Footer from './components/Footer';
+import Sidebar from './components/Sidebar';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <Sidebar />
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow container mx-auto p-4">
+        <Sidebar />
+      </main>
+      <Footer />
     </div>
   );
 };
